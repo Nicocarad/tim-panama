0cdf05e9466e6e4edb2d9b4cb85fe8e1ee93726b
Nicolò Caradonna <nicocarad@gmail.com>
2024-05-06 13:52:25 +0200

    add COMET_ml

--- autoencoder.py
+++ autoencoder.py
@@ -1,3 +1,6 @@
+import comet_ml
+from pytorch_lightning.loggers import CometLogger
+from comet_ml import Experiment
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -5,6 +8,19 @@ from torch.utils.data import DataLoader, random_split
 from torchvision.datasets import MNIST
 import torchvision.transforms as transforms
 import pytorch_lightning as pl
+from torch.utils.tensorboard import SummaryWriter
+import tensorflow as tf
+
+
+# arguments made to CometLogger are passed on to the comet_ml.Experiment class
+comet_logger = CometLogger(api_key="knoxznRgLLK2INEJ9GIbmR7ww")
+Experiment(api_key="knoxznRgLLK2INEJ9GIbmR7ww", log_code=True)
+# Report multiple hyperparameters using a dictionary:
+hyper_params = {
+    "learning_rate": 0.5,
+    "steps": 100000,
+    "batch_size": 50,
+}
 
 class LinearAutoencoder(pl.LightningModule):
     def __init__(self):
@@ -30,6 +46,9 @@ class LinearAutoencoder(pl.LightningModule):
             nn.Sigmoid(),  # Sigmoid per ottenere valori tra 0 e 1
         )
 
+        # Attributo per memorizzare gli output della validazione
+        self.validation_outputs = []
+
     def forward(self, x):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
@@ -53,6 +72,31 @@ class LinearAutoencoder(pl.LightningModule):
         loss = nn.MSELoss()(x_hat, x)
         self.log('test_loss', loss)
 
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        x_hat = self(x)
+        return {'x_hat': x_hat, 'target': x, 'labels': y}
+
+    # def validation_epoch_end(self, outputs):
+    #     # Memorizzazione degli output della validazione
+    #     self.validation_outputs += outputs
+
+    def on_validation_epoch_end(self):
+        # Calcolo della loss della validazione
+        if self.validation_outputs:
+            x_hats = torch.cat([x['x_hat'] for x in self.validation_outputs])
+            targets = torch.cat([x['target'] for x in self.validation_outputs])
+            val_loss = nn.MSELoss()(x_hats, targets)
+            self.log('val_loss', val_loss)
+
+        # Pulizia degli output della validazione
+        self.validation_outputs = []
+
+
+
+
+print("INIZIO")
 # Preparazione dei dati
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -66,10 +110,19 @@ test_loader = DataLoader(mnist_val, batch_size=64, num_workers=4)
 
 # Creazione del modello e trainer
 autoencoder = LinearAutoencoder()
-trainer = pl.Trainer(max_epochs=10, gpus=1)  # Modifica gpus=0 se non hai una GPU
+# trainer = pl.Trainer(max_epochs=10, logger=pl.loggers.TensorBoardLogger('logs/', name='mnist_autoencoder'))  
+# Add CometLogger to your Trainer
+trainer = pl.Trainer(max_epochs=10, logger=comet_logger)
+
+
+comet_logger.log_hyperparams(hyper_params)
 
 # Allenamento del modello
-trainer.fit(autoencoder, train_loader)
+trainer.fit(autoencoder, train_loader, val_dataloaders=test_loader)
 
 # Valutazione del modello
 trainer.test(autoencoder, test_loader)
+
+# sess.graph contains the graph definition; that enables the Graph Visualizer.
+
+# file_writer = tf.summary.FileWriter('logs/', sess.graph)
