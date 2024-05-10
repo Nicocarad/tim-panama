9cd03ba892026eca2ccef42f77cdb20ccc00e606
Nicolò Caradonna <nicocarad@gmail.com>
2024-05-10 17:22:35 +0200

    up

--- Autoencoders/Autoencoder/train_auto.py
+++ Autoencoders/Autoencoder/train_auto.py
@@ -3,7 +3,7 @@ from pytorch_lightning.loggers import CometLogger
 from torch.utils.data import Subset, DataLoader
 from TIMCL import TIMCL
 import pytorch_lightning as pl
-from model import LinearAutoencoder
+from model_auto import LinearAutoencoder
 from sklearn.model_selection import train_test_split
 import torch
 
@@ -19,7 +19,7 @@ comet_logger = CometLogger(
 hyper_params = {
     "learning_rate": 1e-3,
     "batch_size": 64,
-    "epochs": 30,
+    "epochs": 1,
     "input_size": 87,
     "cutting_threshold": 0.5,
     "optimizer": "Adam"
@@ -29,24 +29,35 @@ comet_logger.log_hyperparams(hyper_params)
 
 
 # Creazione del dataset originale
+from sklearn.model_selection import train_test_split
+
 original_dataset = TIMCL("result_df_gt_2.parquet")
 
 indexes = range(0, len(original_dataset))
-splitting = train_test_split(
+
+
+train_indexes, temp_indexes = train_test_split(
     indexes,
-    train_size=0.75,
+    train_size=0.7,
     random_state=42,
     shuffle=True,
 )
-train_indexes = splitting[0]
-val_indexes = splitting[1]
+
+val_indexes, test_indexes = train_test_split(
+    temp_indexes,
+    train_size=0.5,
+    random_state=42,
+    shuffle=True,
+)
 
 # Creazione dei subset utilizzando il dataset originale
 train_dataset = Subset(original_dataset, train_indexes)
 val_dataset = Subset(original_dataset, val_indexes)
+test_dataset = Subset(original_dataset, test_indexes)
 
 print("Train dataset length: ", len(train_dataset))
 print("Val dataset length: ", len(val_dataset))
+print("Test dataset length: ", len(test_dataset))
 
 torch.manual_seed(42)
 
@@ -57,7 +68,7 @@ train_loader = DataLoader(
     drop_last=True,
     shuffle=True,
 )
-test_loader = DataLoader(
+val_loader = DataLoader(
     val_dataset,
     batch_size=hyper_params["batch_size"],
     num_workers=1,
@@ -65,16 +76,23 @@ test_loader = DataLoader(
     shuffle=True,
 )
 
-print("Expected data per epoch: ", len(train_loader) * hyper_params["batch_size"])
-print("Expected data per epoch: ", len(test_loader) * hyper_params["batch_size"])
+test_loader = DataLoader(
+    test_dataset,
+    batch_size=hyper_params["batch_size"],
+    num_workers=1,
+    drop_last=True,
+    shuffle=True,
+)
+
+
 
 # Creazione del modello e trainer
 autoencoder = LinearAutoencoder(hyper_params)
 # Add CometLogger to your Trainer
-trainer = pl.Trainer(max_epochs=hyper_params["epochs"], logger=comet_logger)
+trainer = pl.Trainer(max_epochs=hyper_params["epochs"], logger=comet_logger, default_root_dir="Checkpoints/")
 
 # Allenamento del modello
-trainer.fit(autoencoder, train_loader, val_dataloaders=test_loader)
+trainer.fit(autoencoder, train_loader, val_dataloaders=val_loader)
 
 # Valutazione del modello
 trainer.test(autoencoder, test_loader)
